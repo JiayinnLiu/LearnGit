41b957354041d63a4ac38b25905d8f0c9e9cf876
jiayin <jliu240@ucsc.edu>
2020-09-27 00:42:42 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnGitDemo
+//
+//  Created by 刘嘉殷 on 9/27/20.
+//  Copyright © 2020 liujiayin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
